407a413bba75aae1bd39ebb2ee9b10529fed4205
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-10 11:43:28 +0700

    create _partials view

--- application/views/admin/customers/customers_list.php
+++ application/views/admin/customers/customers_list.php
@@ -9,127 +9,24 @@
 
 <html lang="en">
     <head>
-    <?php $this->load->view('_layouts/head'); ?>
-    <link rel="stylesheet" type="text/css" href="/vendors/DataTables/datatables.min.css"/>
-    <link rel="stylesheet" type="text/css" href="/vendors/DataTables/Select-1.3.3/css/select.bootstrap4.min.css"/>
-    <link rel="stylesheet" type="text/css" href="/vendors/DataTables/Select-1.3.3/css/select.dataTables.min.css"/>
+        <?php $this->load->view('_layouts/head'); ?>
+        <link rel="stylesheet" type="text/css" href="/vendors/DataTables/datatables.min.css"/>
+        <link rel="stylesheet" type="text/css" href="/vendors/DataTables/Select-1.3.3/css/select.bootstrap4.min.css"/>
+        <link rel="stylesheet" type="text/css" href="/vendors/DataTables/Select-1.3.3/css/select.dataTables.min.css"/>
     </head>
     <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
     <?php $this->load->view('_layouts/header'); ?>
 
         <div class="app-body" id="pjax-container">
-
-            <?php $this->load->view('_layouts/head'); ?>
     
-            <?php $this->load->view('_layouts/sidebar'); ?>
+        <?php $this->load->view('_layouts/sidebar'); ?>
     
             <main class="main" >
-                <!-- Breadcrumb-->
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">Home</li>
-                    <li class="breadcrumb-item">
-                        <a href="#">Admin</a>
-                    </li>
-                    <li class="breadcrumb-item active">Dashboard</li>
-                    <!-- Breadcrumb Menu-->
-                    <li class="breadcrumb-menu d-md-down-none">
-                        <div class="btn-group" role="group" aria-label="Button group">
-                            <a class="btn" href="#">
-                                <i class="icon-speech"></i>
-                            </a>
-                            <a class="btn" href="./">
-                                <i class="icon-graph"></i>  Dashboard</a>
-                            <a class="btn" href="#">
-                                <i class="icon-settings"></i>  Settings</a>
-                        </div>
-                    </li>
-                </ol>
+            <?php $this->load->view('_layouts/breadcrumb'); ?>
+
                 <div class="container-fluid">
                     <div class="animated fadeIn">
-                        <div class="row">
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="card text-white bg-primary">
-                                    <div class="card-body pb-0">
-                                        <div class="btn-group float-right">
-                                            <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="icon-settings"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </div>
-                                        <div class="text-value">9.823</div>
-                                        <div>Members online</div>
-                                    </div>
-                                    <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
-                                        <canvas class="chart" id="card-chart1" height="70"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="card text-white bg-info">
-                                    <div class="card-body pb-0">
-                                        <button class="btn btn-transparent p-0 float-right" type="button">
-                                            <i class="icon-location-pin"></i>
-                                        </button>
-                                        <div class="text-value">9.823</div>
-                                        <div>Members online</div>
-                                    </div>
-                                    <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
-                                        <canvas class="chart" id="card-chart2" height="70"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="card text-white bg-warning">
-                                    <div class="card-body pb-0">
-                                        <div class="btn-group float-right">
-                                            <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="icon-settings"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </div>
-                                        <div class="text-value">9.823</div>
-                                        <div>Members online</div>
-                                    </div>
-                                    <div class="chart-wrapper mt-3" style="height:70px;">
-                                        <canvas class="chart" id="card-chart3" height="70"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="card text-white bg-danger">
-                                    <div class="card-body pb-0">
-                                        <div class="btn-group float-right">
-                                            <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="icon-settings"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </div>
-                                        <div class="text-value">9.823</div>
-                                        <div>Members online</div>
-                                    </div>
-                                    <div class="chart-wrapper mt-3 mx-3" style="height:70px;">
-                                        <canvas class="chart" id="card-chart4" height="70"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                        </div>
-                        <!-- /.row-->
+                    <?php $this->load->view('_partials/content_top'); ?>
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
@@ -217,93 +114,7 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- /.card-->
-                        <div class="row">
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="brand-card">
-                                    <div class="brand-card-header bg-facebook">
-                                        <i class="fa fa-facebook"></i>
-                                        <div class="chart-wrapper">
-                                            <canvas id="social-box-chart-1" height="90"></canvas>
-                                        </div>
-                                    </div>
-                                    <div class="brand-card-body">
-                                        <div>
-                                            <div class="text-value">89k</div>
-                                            <div class="text-uppercase text-muted small">friends</div>
-                                        </div>
-                                        <div>
-                                            <div class="text-value">459</div>
-                                            <div class="text-uppercase text-muted small">feeds</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="brand-card">
-                                    <div class="brand-card-header bg-twitter">
-                                        <i class="fa fa-twitter"></i>
-                                        <div class="chart-wrapper">
-                                            <canvas id="social-box-chart-2" height="90"></canvas>
-                                        </div>
-                                    </div>
-                                    <div class="brand-card-body">
-                                        <div>
-                                            <div class="text-value">973k</div>
-                                            <div class="text-uppercase text-muted small">followers</div>
-                                        </div>
-                                        <div>
-                                            <div class="text-value">1.792</div>
-                                            <div class="text-uppercase text-muted small">tweets</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="brand-card">
-                                    <div class="brand-card-header bg-linkedin">
-                                        <i class="fa fa-linkedin"></i>
-                                        <div class="chart-wrapper">
-                                            <canvas id="social-box-chart-3" height="90"></canvas>
-                                        </div>
-                                    </div>
-                                    <div class="brand-card-body">
-                                        <div>
-                                            <div class="text-value">500+</div>
-                                            <div class="text-uppercase text-muted small">contacts</div>
-                                        </div>
-                                        <div>
-                                            <div class="text-value">292</div>
-                                            <div class="text-uppercase text-muted small">feeds</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="brand-card">
-                                    <div class="brand-card-header bg-google-plus">
-                                        <i class="fa fa-google-plus"></i>
-                                        <div class="chart-wrapper">
-                                            <canvas id="social-box-chart-4" height="90"></canvas>
-                                        </div>
-                                    </div>
-                                    <div class="brand-card-body">
-                                        <div>
-                                            <div class="text-value">894</div>
-                                            <div class="text-uppercase text-muted small">followers</div>
-                                        </div>
-                                        <div>
-                                            <div class="text-value">92</div>
-                                            <div class="text-uppercase text-muted small">circles</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.col-->
-                        </div>
+                    <?php $this->load->view('_partials/content_bottom'); ?>
                         
                     </div>
                 </div>
@@ -311,31 +122,8 @@
             
             <?php $this->load->view('_layouts/aside'); ?>
         </div>
-        <footer class="app-footer">
-            <div>
-                <a href="https://coreui.io">CoreUI</a>
-                <span>&copy; 2018 creativeLabs.</span>
-            </div>
-            <div class="ml-auto">
-                <span>Powered by</span>
-                <a href="https://coreui.io">CoreUI</a>
-            </div>
-        </footer>
-        <!-- CoreUI and necessary plugins-->
-        <script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/popper.js/dist/umd/popper.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/pace-progress/pace.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/@coreui/coreui/dist/js/coreui.min.js'); ?>"></script>
-        <!-- Plugins and scripts required by this view-->
-        <script src="<?php echo base_url('node_modules/chart.js/dist/Chart.min.js'); ?>"></script>
-        <script src="<?php echo base_url('node_modules/@coreui/coreui-plugin-chartjs-custom-tooltips/dist/js/custom-tooltips.min.js'); ?>"></script>
-        <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
-        <script type="text/javascript" src="/vendors/DataTables/datatables.min.js"></script>
-        
-        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.pjax.js'); ?>"></script>
-
+        <?php $this->load->view('_layouts/footer'); ?>        
+        <?php $this->load->view('_layouts/script'); ?>
 
         <script type="text/javascript" src="/vendors/DataTables/datatables.min.js"></script>
         
