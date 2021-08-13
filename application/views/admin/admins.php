95f8c40d649c9cf2562cac0234515b27b019922b
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-13 14:42:18 +0700

    update modules

--- application/views/admin/admins.php
+++ application/views/admin/admins.php
@@ -8,118 +8,122 @@
 -->
 
 <html lang="en">
-    <head>
-        <?php $this->load->view('_layouts/head'); ?>
 
-    </head>
-    <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+<head>
+    <?php $this->load->view('_layouts/head'); ?>
+
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
     <?php $this->load->view('_layouts/header'); ?>
 
-        <div class="app-body" id="pjax-container">
-    
+    <div class="app-body" id="pjax-container">
+
         <?php $this->load->view('_layouts/sidebar'); ?>
-    
-            <main class="main" >
+
+        <main class="main">
             <?php $this->load->view('_layouts/breadcrumb'); ?>
 
-                <div class="container-fluid">
-                    <div class="animated fadeIn">
-
-                    <div class="card">        
-                            <div class="card-header">
-                                <div class="d-flex justify-content-between bd-highlight">
-                                    <div class="p-2 bd-highlight"><a href="<?php echo base_url($URI.'/create'); ?>" class="btn btn-success btn-sm"> Create</a></div>
-                                    <div class="p-2 bd-highlight"> <h5>Manage Admin</h5></div>
+            <div class="container-fluid">
+                <div class="animated fadeIn">
+                    <div class="card">
+                        <?php $this->load->view('_layouts/alerts'); ?>
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between bd-highlight">
+                                <div class="p-2 bd-highlight"><a href="<?php echo base_url($URI . '/create'); ?>" class="btn btn-success btn-sm"> Create</a></div>
+                                <div class="p-2 bd-highlight">
+                                    <h5>Manage Admin</h5>
                                 </div>
                             </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-sm-5">
-                                        <h4 class="card-title mb-0"><?php echo $title; ?></h4>
-                                        <div class="small text-muted">November 2017</div>
-                                    </div>
-                                    <!-- /.col-->
-                                    <div class="col-sm-7 d-none d-md-block">
-                                        <button class="btn btn-primary float-right" type="button">
-                                            <i class="icon-cloud-download"></i>
-                                        </button>
-                                        <div class="btn-group btn-group-toggle float-right mr-3" data-toggle="buttons">
-                                            <label class="btn btn-outline-secondary">
-                                                <input id="option1" type="radio" name="options" autocomplete="off"> Day
-                                            </label>
-                                            <label class="btn btn-outline-secondary active">
-                                                <input id="option2" type="radio" name="options" autocomplete="off" checked=""> Month
-                                            </label>
-                                            <label class="btn btn-outline-secondary">
-                                                <input id="option3" type="radio" name="options" autocomplete="off"> Year
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <!-- /.col-->
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-5">
+                                    <h4 class="card-title mb-0"><?php echo $title; ?></h4>
+                                    <div class="small text-muted">November 2017</div>
                                 </div>
-                                <!-- /.row-->
-                                
-                                <div class="table-responsive">
+                                <!-- /.col-->
+                                <div class="col-sm-7 d-none d-md-block">
+                                    <button class="btn btn-primary float-right" type="button">
+                                        <i class="icon-cloud-download"></i>
+                                    </button>
+                                    <div class="btn-group btn-group-toggle float-right mr-3" data-toggle="buttons">
+                                        <label class="btn btn-outline-secondary">
+                                            <input id="option1" type="radio" name="options" autocomplete="off"> Day
+                                        </label>
+                                        <label class="btn btn-outline-secondary active">
+                                            <input id="option2" type="radio" name="options" autocomplete="off" checked=""> Month
+                                        </label>
+                                        <label class="btn btn-outline-secondary">
+                                            <input id="option3" type="radio" name="options" autocomplete="off"> Year
+                                        </label>
+                                    </div>
+                                </div>
+                                <!-- /.col-->
+                            </div>
+                            <!-- /.row-->
 
-                                    <table class="table table-bordered table-striped" id="mytable">
-                                        <thead>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped" id="mytable">
+                                    <thead>
                                         <tr>
                                             <th>Name</th>
                                             <th>Status</th>
                                             <th>Role</th>
                                             <th>Actions</th>
                                         </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php if (count($items) > 0): foreach ($items as $item): ?>
-                                            <tr>
-                                                <td><?php echo $item->name; ?></td>
-                                                <td><?php echo ucfirst($item->status); ?></td>
-                                                <td><?php echo $item->group_name; ?></td>
-                                                <td>
-                                                    <form action="<?php echo base_url($URI.'/delete/' . $item->id); ?>"
-                                                        method="post" onsubmit="return confirm('Are you sure?');">
-                                                        <div class="btn-group btn-group-sm">
-                                                            <a href="<?php echo base_url($URI.'/edit/' . $item->id); ?>"
-                                                            class="btn btn-info"> Edit</a>
-                                                            <button type="submit" class="btn btn-danger" <?php if($item->id===$this->smarty_acl->get_admin()['id']){ echo 'disabled';} ?>> Delete</button>
-                                                        </div>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        <?php endforeach; else: ?>
+                                    </thead>
+                                    <tbody>
+                                        <?php if (count($items) > 0) : foreach ($items as $item) : ?>
+                                                <tr>
+                                                    <td><?php echo $item->name; ?></td>
+                                                    <td><?php echo ucfirst($item->status); ?></td>
+                                                    <td><?php echo $item->group_name; ?></td>
+                                                    <td>
+                                                        <form action="<?php echo base_url($URI . '/delete/' . $item->id); ?>" method="post" onsubmit="return confirm('Are you sure?');">
+                                                            <div class="btn-group btn-group-sm">
+                                                                <a href="<?php echo base_url($URI . '/edit/' . $item->id); ?>" class="btn btn-info"> Edit</a>
+                                                                <button type="submit" class="btn btn-danger" <?php if ($item->id === $this->smarty_acl->get_admin()['id']) {
+                                                                                                                    echo 'disabled';
+                                                                                                                } ?>> Delete</button>
+                                                            </div>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach;
+                                        else : ?>
                                             <tr>
                                                 <td colspan="4" class="text-center">No results found!</td>
                                             </tr>
                                         <?php endif; ?>
-                                        </tbody>
-                                    </table>
+                                    </tbody>
+                                </table>
 
-                                </div>
                             </div>
-                            <div class="card-footer">
-                                
-                            </div>
-                        </div>                        
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
                     </div>
                 </div>
-            </main>
-            
-            <?php $this->load->view('_layouts/aside'); ?>
-        </div>
-        <?php $this->load->view('_layouts/footer'); ?>        
-        <?php $this->load->view('_layouts/script'); ?>
+            </div>
+        </main>
 
+        <?php $this->load->view('_layouts/aside'); ?>
+    </div>
+    <?php $this->load->view('_layouts/footer'); ?>
+    <?php $this->load->view('_layouts/script'); ?>
 
-        <script type="text/javascript">
-            $(function () {
-                $(document).ajaxComplete(function () {
-                    Pace.restart()
-                });
-                if ($.support.pjax) {
-                    $(document).pjax('a[data-pjax]', '#pjax-container')
-                }
+    <script type="text/javascript">
+        $(function() {
+            $(document).ajaxComplete(function() {
+                Pace.restart()
             });
-        </script>
-    </body>
+            if ($.support.pjax) {
+                $(document).pjax('a[data-pjax]', '#pjax-container')
+            }
+        });
+    </script>
+</body>
+
 </html>
--- application/views/admin/modules_form.php
+++ application/views/admin/modules_form.php
@@ -1,33 +1,93 @@
-<?php $this->load->view('header'); ?>
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v2.1.12
+* @link https://coreui.io
+* Copyright (c) 2018 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
 
-    <div class="container">
-        <div class="row">
-            <div class="col-6 offset-lg-3">
-                <div class="card">
-                    <div class="card-header">
-                        <h5>Module <?php echo isset($item) ? 'Edit' : 'Create' ;?></h5>
-                    </div>
-                    <div class="card-body">
-                        <form action="<?php echo $form_action ;?>" method="post">
-                            <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" name="name" placeholder="Name" class="form-control" value="<?php echo $item->name ?? set_value('name'); ?>"/>
+<html lang="en">
+
+<head>
+    <?php $this->load->view('_layouts/head'); ?>
+
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <?php $this->load->view('_layouts/header'); ?>
+
+    <div class="app-body" id="pjax-container">
+
+        <?php $this->load->view('_layouts/sidebar'); ?>
+
+        <main class="main">
+            <?php $this->load->view('_layouts/breadcrumb'); ?>
+            <div class="container-fluid">
+                <div class="animated fadeIn">
+
+                    <?php $this->load->view('_layouts/alerts'); ?>
+                    <div class="card">
+                        <form action="<?php echo $form_action; ?>" method="post">
+                            <div class="card-header">
+                                <div class="d-flex justify-content-between bd-highlight">
+                                    <div class="p-2 bd-highlight">
+                                        <h4 class="card-title mb-0"><?php echo $title; ?></h4>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5><?php echo isset($item) ? 'Edit' : 'Create'; ?> Modules</h5>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label>Controller Name</label>
-                                <input type="text" name="controller" placeholder="Controller Name" class="form-control" value="<?php echo $item->controller ?? set_value('controller'); ?>"/>
+                            <div class="card-body">
+
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" name="name" placeholder="Name" class="form-control" value="<?php echo $item->name ?? set_value('name'); ?>" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Controller Name</label>
+                                    <input type="text" name="controller" placeholder="Controller Name" class="form-control" value="<?php echo $item->controller ?? set_value('controller'); ?>" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Permissions(methods) List</label>
+                                    <input type="text" name="permissions" placeholder="Permissions" class="form-control" value="<?php echo isset($item) ? implode(',', json_decode($item->permissions, 0)) : set_value('permissions'); ?>" />
+                                    <small class="form-text">Comma separated</small>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label>Permissions(methods) List</label>
-                                <input type="text" name="permissions" placeholder="Permissions" class="form-control" value="<?php echo isset($item) ? implode(',',json_decode($item->permissions,0)) : set_value('permissions'); ?>"/>
-                                <small class="form-text">Comma separated</small>
+                            <div class="card-footer">
+                                <div class="d-flex justify-content-between bd-highlight">
+                                    <div class="p-2 bd-highlight">
+                                        <button type="submit" class="btn btn-primary"><?php echo isset($item) ? 'Update' : 'Create'; ?></button>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <a href="<?php echo site_url('admin/modules') ?>" class="btn btn-default">Batal</a>
+                                    </div>
+                                </div>
                             </div>
-                            <button type="submit" class="btn btn-primary"><?php echo isset($item) ? 'Update' : 'Create' ;?></button>
                         </form>
                     </div>
+
                 </div>
             </div>
-        </div>
-    </div>
+        </main>
 
-<?php $this->load->view('footer'); ?>
+        <?php $this->load->view('_layouts/aside'); ?>
+    </div>
+    <?php $this->load->view('_layouts/footer'); ?>
+    <?php $this->load->view('_layouts/script'); ?>
+
+
+    <script type="text/javascript">
+        $(function() {
+            $(document).ajaxComplete(function() {
+                Pace.restart()
+            });
+            if ($.support.pjax) {
+                $(document).pjax('a[data-pjax]', '#pjax-container')
+            }
+        });
+    </script>
+</body>
+
+</html>
